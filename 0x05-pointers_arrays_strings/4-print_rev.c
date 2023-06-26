ab8078cc538bb59dd5830226fd294e37fc6b6b46
Ahmedabdelhafiz11 <ahmed_abdelhafiz2011@yahoo.com>
2023-06-26 08:00:30 -0700

    Pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,7 +4,18 @@
  * @s: the string to print
  *
  * Return: void
+ *
  */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
 void print_rev(char *s)
 {
 	int i = _strlen(s) - 1;
