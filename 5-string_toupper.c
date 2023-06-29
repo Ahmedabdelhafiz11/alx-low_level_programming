277f8294791f70b4024acdd3e045c77b12875026
Ahmedabdelhafiz11 <ahmed_abdelhafiz2011@yahoo.com>
2023-06-29 11:41:41 -0700

    more pointers

---
+++ 5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @c: input characters
+ *
+ * Return: Pointer to characters
+ */
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] >= 97 && c[i] <= 122)
+		{
+			c[i] = c[i] - 32;
+		}
+		i++;
+	}
+	return (c);
+}
