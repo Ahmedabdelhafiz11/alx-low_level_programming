0c73c59356d0088c3296a55e8ad5b2f5f0f3ade8
Ahmedabdelhafiz11 <ahmed_abdelhafiz2011@yahoo.com>
2023-07-19 09:09:27 -0700

    pf

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,18 +12,19 @@
 int main(int argc, char *argv[])
 {
 	int i;
-
+	char *p = (char *)main;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
+		i = atoi(argv[1]);
 	}
 	if (atoi(argv[1]) < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	for (i = 0; i < atoi(argv[1]); i++)
-		printf("%.2x ", i);
-	printf("\n");
+	while (i--)
+		printf("%02hhx%s", *p++, i ? " " : "\n");
+	return (0);
 }
