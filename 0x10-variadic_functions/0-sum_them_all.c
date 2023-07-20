c6c5d72baf0f1fa381e06f93822a9e15ae05b7bb
Ahmedabdelhafiz11 <ahmed_abdelhafiz2011@yahoo.com>
2023-07-20 12:45:30 -0700

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,24 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * sum_them_all - sum all arguments
- * @n: number of arguments
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
  *
- * Return: sum
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int sum = 0;
-	unsigned int i = 0;
-	va_list l;
+	va_list ap;
+	unsigned int i, sum = 0;
 
-	if (n != 0)
-	{
-		va_start(l, n);
-		while (i < n)
-		{
-			sum += va_arg(l, unsigned int);
-			i++;
-		}
-	}
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
 	return (sum);
-}
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,90 +1,50 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
 
 /**
- * print_char - function that prints character
- * @l char specifier
- */
-
-void print_char(va_list l)
-{
-	char c;
-
-	c = va_arg(l, int);
-	printf("%c", c);
-}
-
-/**
- * print_int - function that prints integer
- * @l: integer specifier
- */
-
-void print_int(va_list l)
-{
-	int i;
-
-	ni = va_arg(l, int);
-	printf("%i", i);
-}
-
-/**
- * print_float - function that prints a float
- * @l: flat specifier
- */
-
-void print_float(va_list l)
-{
-	float f;
-
-	f = va_arg(l, double);
-	printf("%f", f);
-}
-
-/**
- * print_string - function that prints string
- * @l: string specifier
- */
-
-void print_string(va_list argptr)
-{
-	char *str;
-
-	str = va_arg(argptr, char*);
-
-	if (str == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
-}
-
-/**
- * print_all - print all input
- * @format: format to follow
- *
- * Return: void
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
-	va_list l;
-	char *s = ", ";
-	print_type types[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string}
-	};
+	int i = 0;
+	char *str, *sep = "";
 
-	while (format != '\0')
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
 	{
-		i++;
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
 	}
-	va_start(l, n);
-	
-	if (separator && (i < n - 1))
-		printf("%s", separator);
-	va_end(l);
+
 	printf("\n");
+	va_end(list);
 }
