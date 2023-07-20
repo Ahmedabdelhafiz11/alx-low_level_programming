422475f6efbb02fb230351e7b2019c33ab07c759
Ahmedabdelhafiz11 <ahmed_abdelhafiz2011@yahoo.com>
2023-07-20 12:41:22 -0700

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,35 +1,25 @@
 #include "variadic_functions.h"
 
 /**
- *  sum_them_all - Sum all params
+ * sum_them_all - sum all arguments
+ * @n: number of arguments
  *
- *  @n: const unsigned int
- *
- *  Return: int sum
+ * Return: sum
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int sum = 0;
+	unsigned int i = 0;
+	va_list l;
 
-	va_list ap;
-
-	unsigned int i;
-	int sum = 0;
-
-	va_start(ap, n);
-
-	if (n == 0)
+	if (n != 0)
 	{
-		return (0);
+		va_start(l, n);
+		while (i < n)
+		{
+			sum += va_arg(l, unsigned int);
+			i++;
+		}
 	}
-
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(ap, unsigned int);
-	}
-
-	va_end(ap);
-
 	return (sum);
-
 }
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,90 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 
 /**
- *  print_all - Prints anything
- *
- *  @format: List of args passed to function
- *
- *  Return: void
+ * print_char - function that prints character
+ * @l char specifier
  */
 
-void print_all(const char * const format, ...)
+void print_char(va_list l)
+{
+	char c;
+
+	c = va_arg(l, int);
+	printf("%c", c);
+}
+
+/**
+ * print_int - function that prints integer
+ * @l: integer specifier
+ */
+
+void print_int(va_list l)
+{
+	int i;
+
+	ni = va_arg(l, int);
+	printf("%i", i);
+}
+
+/**
+ * print_float - function that prints a float
+ * @l: flat specifier
+ */
+
+void print_float(va_list l)
+{
+	float f;
+
+	f = va_arg(l, double);
+	printf("%f", f);
+}
+
+/**
+ * print_string - function that prints string
+ * @l: string specifier
+ */
+
+void print_string(va_list argptr)
 {
 	char *str;
-	unsigned int i = 0, commaCheck = 0;
-	va_list ap;
 
-	va_start(ap, format);
+	str = va_arg(argptr, char*);
 
-	while (format && format[i])
+	if (str == NULL)
 	{
-		if (commaCheck)
-			printf(", ");
-
-		switch (format[i])
-		{
-		case 'c':
-			printf("%c", va_arg(ap, int));
-			break;
-		case 'i':
-			printf("%i", va_arg(ap, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(ap, double));
-			break;
-		case 's':
-			str = va_arg(ap, char*);
-			if (str)
-			{
-				printf("%s", str);
-				break;
-			}
-			printf("(nil)");
-			break;
-		default:
-			commaCheck = 0;
-			i++;
-			continue;
-		}
-		commaCheck = 1,	i++;
+		printf("(nil)");
+		return;
 	}
-	putchar('\n');
-	va_end(ap);
+	printf("%s", str);
+}
+
+/**
+ * print_all - print all input
+ * @format: format to follow
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0;
+	va_list l;
+	char *s = ", ";
+	print_type types[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	while (format != '\0')
+	{
+		i++;
+	}
+	va_start(l, n);
+	
+	if (separator && (i < n - 1))
+		printf("%s", separator);
+	va_end(l);
+	printf("\n");
 }
