fa7e641c4aeb87469ef07ad666941f632947afe2
Ahmedabdelhafiz11 <ahmed_abdelhafiz2011@yahoo.com>
2023-07-27 13:43:40 -0700

    lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,20 +1,19 @@
-#ifndef LISTS_H
-#define LISTS_H
-#include <stdlib>
-/*
- * Main File
- * Descreption: Header file containing prototypes and definitions for all functions
- *
-*/
+#ifndef LISTS
+#define LISTS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 
 /**
- * struct list_s - linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list
- *          
+ * Description: singly linked list node structure
+ *
 */
 typedef struct list_s
 {
@@ -23,10 +22,12 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/*Prototypes*/
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _putchar(char c);
 
 #endif
